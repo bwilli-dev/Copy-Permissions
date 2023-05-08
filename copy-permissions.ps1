15830a4765c73e2811f1a9acc930b6d18b368797
Ben Williamson <54720451+bwilli-dev@users.noreply.github.com>
2023-05-08 14:11:30 -0500

    Create copy-permissions.ps1
    basic commit

---
+++ copy-permissions.ps1
@@ -0,0 +1,15 @@
+#langugae: PowerShell
+#Copy ACL(Security Permissions) from one folder to another
+
+$source_path = "path\to\source"
+$destination_path = "path\to\destination"
+
+
+#list all folders from the source path
+$source_folders = Get-ChildItem -Path $source_path -Directory
+
+foreach ($i in $source_folders){
+    $name = $i.Name
+    Get-Acl -Path $source_path\$name | Set-Acl -Path "$destination_path\$name"
+}
+
